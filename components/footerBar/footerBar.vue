ba31305b8436dd42b8fba869bba941531ec4c61a
zhangxu1161 <zhangxu1161@gmail.com>
2020-07-13 14:37:41 +0800

    feat: modified email

--- components/footerBar/footerBar.vue
+++ components/footerBar/footerBar.vue
@@ -4,7 +4,7 @@
     <div class="footer-wrapper container">
       <b-row class="mail-wrapper">
         <b-col class="mail-container" cols="8" sm="7" md="9" xl="9" lg="9">
-          Crustcommunity@gmail.com
+          CrustNetwork@gmail.com
         </b-col>
         <b-col cols="4" sm="5" md="3" xl="3" lg="3">
           <b-button class="btn" variant="warning" @click="handleCopy">{{
@@ -75,7 +75,7 @@ export default {
   },
   methods: {
     handleCopy() {
-      this.$copyText("Crustcommunity@gmail.com")
+      this.$copyText("CrustNetwork@gmail.com")
         .then(() => {
           this.$bvToast.toast(this.$t("footer.Success"), {
             title: false,
