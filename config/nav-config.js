79b0fb204ab3a9e772666a4aef9a347d03a23c80
Deyang Liu <ldy0909061@gmail.com>
2020-12-04 16:00:48 +0800

    fix csm issue

--- config/nav-config.js
+++ config/nav-config.js
@@ -13,7 +13,7 @@ const innerList = [
 // }
 const outerDit = {
   "join profit ark": "https://ark.crust.network/",
-  "cmstoken": "https://csm-event.crust.run/#/cru/auto",
+  "csmtoken": "https://csm-event.crust.run/#/cru/auto",
   github: "https://github.com/crustio",
   twitter: "https://twitter.com/CrustNetwork",
   medium: "https://medium.com/@CrustNetwork",
